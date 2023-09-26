6e5cd4db32821377e65bc4a63920328ba6ffa29f
Viathorr <142295754+Viathorr@users.noreply.github.com>
2023-09-26 15:13:48 +0300

    books pq

--- lab1/main.cc
+++ lab1/main.cc
@@ -7,123 +7,123 @@ int main() {
   srand(time(NULL));
 
   std::cout << "\n~~~ Priority queue implemented by linked list: \n\n";
-  mypriorityqueue::Priority_queue_linked_list<std::vector<int>> vectorPQ(
-      mycomparator::comparatorVector<int>);
-  vectorPQ.GenerateRandomData();
-  std::cout << "PQ size: " << vectorPQ.GetSize() << ", data: \n";
-  vectorPQ.Print();
-  vectorPQ.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  std::cout << "Peek: " << vectorPQ.Peek()
-            << ", size after 3 insertions: " << vectorPQ.GetSize()
+  mypriorityqueue::Priority_queue_linked_list<mybook::Book> bookPQ(
+      mycomparator::comparatorBook);
+  bookPQ.GenerateRandomData();
+  std::cout << "PQ size: " << bookPQ.GetSize() << ", data: \n";
+  bookPQ.Print();
+  bookPQ.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  std::cout << "Peek: " << bookPQ.Peek()
+            << ", size after 3 insertions: " << bookPQ.GetSize()
             << ", data: " << std::endl;
-  vectorPQ.Print();
-  vectorPQ.Dequeue();
-  vectorPQ.Dequeue();
-  vectorPQ.Dequeue();
-  vectorPQ.Dequeue();
-  std::cout << "PQ size after 4 removals: " << vectorPQ.GetSize()
-            << ", data: \n";
-  vectorPQ.Print();
+  bookPQ.Print();
+  bookPQ.Dequeue();
+  bookPQ.Dequeue();
+  bookPQ.Dequeue();
+  bookPQ.Dequeue();
+  std::cout << "PQ size after 4 removals: " << bookPQ.GetSize() << ", data: \n";
+  bookPQ.Print();
 
   std::cout << "\n~~~ Priority queue implemented by array: \n\n";
-  mypriorityqueue::Priority_queue_array<std::vector<int>> vectorPQ1(
-      mycomparator::comparatorVector<int>);
-  vectorPQ1.GenerateRandomData();
-  std::cout << "PQ size: " << vectorPQ1.GetSize() << ", data: \n";
-  vectorPQ1.Print();
-  vectorPQ1.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ1.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ1.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  std::cout << "Peek: " << vectorPQ1.Peek()
-            << ", size after 3 insertions: " << vectorPQ1.GetSize()
+  mypriorityqueue::Priority_queue_array<mybook::Book> bookPQ1(
+      mycomparator::comparatorBook);
+  bookPQ1.GenerateRandomData();
+  std::cout << "PQ size: " << bookPQ1.GetSize() << ", data: \n";
+  bookPQ1.Print();
+  bookPQ1.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ1.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ1.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  std::cout << "Peek: " << bookPQ1.Peek()
+            << ", size after 3 insertions: " << bookPQ1.GetSize()
             << ", data: " << std::endl;
-  vectorPQ1.Print();
-  vectorPQ1.Dequeue();
-  vectorPQ1.Dequeue();
-  vectorPQ1.Dequeue();
-  vectorPQ1.Dequeue();
-  std::cout << "PQ size after 4 removals: " << vectorPQ1.GetSize()
+  bookPQ1.Print();
+  bookPQ1.Dequeue();
+  bookPQ1.Dequeue();
+  bookPQ1.Dequeue();
+  bookPQ1.Dequeue();
+  std::cout << "PQ size after 4 removals: " << bookPQ1.GetSize()
             << ", data: \n";
-  vectorPQ1.Print();
+  bookPQ1.Print();
 
   std::cout << "\n~~~ Priority queue implemented by binary heap: \n\n";
-  mypriorityqueue::Priority_queue_binary_heap<std::vector<int>> vectorPQ2(
-      mycomparator::comparatorVector<int>);
-  vectorPQ2.GenerateRandomData();
-  std::cout << "PQ size: " << vectorPQ2.GetSize() << ", data: \n";
-  vectorPQ2.Print();
-  vectorPQ2.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ2.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ2.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  std::cout << "Peek: " << vectorPQ2.Peek()
-            << ", size after 3 insertions: " << vectorPQ2.GetSize()
+  mypriorityqueue::Priority_queue_binary_heap<mybook::Book> bookPQ2(
+      mycomparator::comparatorBook);
+  bookPQ2.GenerateRandomData();
+  std::cout << "PQ size: " << bookPQ2.GetSize() << ", data: \n";
+  bookPQ2.Print();
+  bookPQ2.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ2.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ2.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  std::cout << "Peek: " << bookPQ2.Peek()
+            << ", size after 3 insertions: " << bookPQ2.GetSize()
             << ", data: " << std::endl;
-  vectorPQ2.Print();
-  vectorPQ2.Dequeue();
-  vectorPQ2.Dequeue();
-  vectorPQ2.Dequeue();
-  vectorPQ2.Dequeue();
-  std::cout << "PQ size after 4 removals: " << vectorPQ2.GetSize()
+  bookPQ2.Print();
+  bookPQ2.Dequeue();
+  bookPQ2.Dequeue();
+  bookPQ2.Dequeue();
+  bookPQ2.Dequeue();
+  std::cout << "PQ size after 4 removals: " << bookPQ2.GetSize()
             << ", data: \n";
-  vectorPQ2.Print();
+  bookPQ2.Print();
 
-  std::cout << "\n~~~ Priority queue implemented by binary (search) tree:\n\n";
-  mypriorityqueue::Priority_queue_AVL_tree<std::vector<int>> vectorPQ3(
-      mycomparator::comparatorVector<int>);
-  vectorPQ3.GenerateRandomData();
-  std::cout << "PQ size: " << vectorPQ3.GetSize() << ", data: \n";
-  vectorPQ3.Print();
-  vectorPQ3.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ3.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ3.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  std::cout << "Peek: " << vectorPQ3.Peek()
-            << ", size after 3 insertions: " << vectorPQ3.GetSize()
+  std::cout << "\n~~~ Priority queue implemented by binary (search) tree: \n\n";
+  mypriorityqueue::Priority_queue_AVL_tree<mybook::Book> bookPQ3(
+      mycomparator::comparatorBook);
+  bookPQ3.GenerateRandomData();
+  std::cout << "PQ size: " << bookPQ3.GetSize() << ", data: \n";
+  bookPQ3.Print();
+  bookPQ3.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ3.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ3.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  std::cout << "Peek: " << bookPQ3.Peek()
+            << ", size after 3 insertions: " << bookPQ3.GetSize()
             << ", data: " << std::endl;
-  vectorPQ3.Print();
-  vectorPQ3.Dequeue();
-  vectorPQ3.Dequeue();
-  vectorPQ3.Dequeue();
-  vectorPQ3.Dequeue();
-  std::cout << "PQ size after 4 removals: " << vectorPQ3.GetSize()
+  bookPQ3.Print();
+  bookPQ3.Dequeue();
+  bookPQ3.Dequeue();
+  bookPQ3.Dequeue();
+  bookPQ3.Dequeue();
+  std::cout << "PQ size after 4 removals: " << bookPQ3.GetSize()
             << ", data: \n";
-  vectorPQ3.Print();
+  bookPQ3.Print();
 
   std::cout << "\n~~~ Priority queue implemented by AVL tree: \n\n";
-  mypriorityqueue::Priority_queue_AVL_tree<std::vector<int>> vectorPQ4(
-      mycomparator::comparatorVector<int>);
-  vectorPQ4.GenerateRandomData();
-  std::cout << "PQ size: " << vectorPQ4.GetSize() << ", data: \n";
-  vectorPQ4.Print();
-  vectorPQ4.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ4.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  vectorPQ4.Enqueue(
-      myrandomdatagenerator::Random_data_generator::GetRandomVectorData<int>());
-  std::cout << "Peek: " << vectorPQ4.Peek()
-            << ", size after 3 insertions: " << vectorPQ4.GetSize()
+  mypriorityqueue::Priority_queue_AVL_tree<mybook::Book> bookPQ4(
+      mycomparator::comparatorBook);
+  bookPQ4.GenerateRandomData();
+  std::cout << "PQ size: " << bookPQ4.GetSize() << ", data: \n";
+  bookPQ4.Print();
+  bookPQ4.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ4.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  bookPQ4.Enqueue(
+      myrandomdatagenerator::Random_data_generator::GetRandomBookData());
+  std::cout << "Peek: " << bookPQ4.Peek()
+            << ", size after 3 insertions: " << bookPQ4.GetSize()
             << ", data: " << std::endl;
-  vectorPQ4.Print();
-  vectorPQ4.Dequeue();
-  vectorPQ4.Dequeue();
-  vectorPQ4.Dequeue();
-  vectorPQ4.Dequeue();
-  std::cout << "PQ size after 4 removals: " << vectorPQ4.GetSize()
+  bookPQ4.Print();
+  bookPQ4.Dequeue();
+  bookPQ4.Dequeue();
+  bookPQ4.Dequeue();
+  bookPQ4.Dequeue();
+  std::cout << "PQ size after 4 removals: " << bookPQ4.GetSize()
             << ", data: \n";
-  vectorPQ4.Print();
+  bookPQ4.Print();
+
   return 0;
 }
