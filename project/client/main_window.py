1ae3994b1e361b1c794536ff7e7692c9bbfaaf5d
Viathorr <142295754+Viathorr@users.noreply.github.com>
2024-02-01 17:01:15 +0200

    added docstrings to main window class

--- project/client/main_window.py
+++ project/client/main_window.py
@@ -8,7 +8,50 @@ from client.chat_window import ClientChatWindow
 
 
 class MainWindow:
-    def __init__(self, parent, theme='minty'):
+    """
+    Class representing the main window of the chat application.
+
+    Attributes:
+        master (ChatApp): The application object.
+        root (ttk.Window): The main window of the application.
+        login_btn (ttk.Button): Button to open the login window.
+        signup_btn (ttk.Button): Button to open the signup window.
+        settings_window (Settings): Reference to the settings window.
+
+    Methods:
+        open():
+            Opens the main window.
+        close_window():
+            Closes the main window.
+        reopen_window():
+            Reopens the main window after being minimized.
+        _open_chat_window():
+            Opens the chat window if the user is logged in.
+        _close_chat_window():
+            Closes the chat window.
+        _open_login_window():
+            Opens the login window.
+        _close_login_window():
+            Closes the login window.
+        _open_signup_window():
+            Opens the signup window.
+        _close_signup_window():
+            Closes the signup window.
+        _open_settings():
+            Opens the settings window.
+        _close_settings_window():
+            Closes the settings window.
+        change_buttons_state(disable: bool):
+            Changes the state of login and signup buttons based on the user's login status.
+    """
+    def __init__(self, parent, theme='minty') -> None:
+        """
+        Initialize the main window of the chat application.
+
+        Args:
+            parent (ChatApp): The parent application object.
+            theme (str, optional): The theme of the window. Defaults to 'minty'.
+        """
         self.master = parent  # chat app
 
         # Main Window
@@ -63,16 +106,28 @@ class MainWindow:
 
         self.settings_window = None
 
-    def open(self):
+    def open(self) -> None:
+        """
+        Open the main window.
+        """
         self.root.mainloop()
 
-    def close_window(self):
+    def close_window(self) -> None:
+        """
+        Close the main window.
+        """
         self.root.withdraw()
 
-    def reopen_window(self):
+    def reopen_window(self) -> None:
+        """
+        Reopen the main window after being minimized.
+        """
         self.root.deiconify()
 
-    def _open_chat_window(self):
+    def _open_chat_window(self) -> None:
+        """
+        Open the chat window.
+        """
         if not self.master.user:
             messagebox.showwarning('You must be logged in', "Please log in or sign up first.")
         else:
@@ -86,48 +141,75 @@ class MainWindow:
                 self.root.deiconify()
                 messagebox.showerror(title='Error', message=str(ex))
 
-    def _close_chat_window(self):
+    def _close_chat_window(self) -> None:
+        """
+        Close the chat window.
+        """
         print('in close window method')
         self.chat_window.socket.close()
         self.chat_window.destroy()
         self.root.deiconify()
 
-    def _open_login_window(self):
+    def _open_login_window(self) -> None:
+        """
+        Open the login window
+        """
         self.root.withdraw()
         self.login_window = LogIn(self)
         self.login_window.root.protocol('WM_DELETE_WINDOW', self._close_login_window)
         self.login_window.open()
 
-    def _close_login_window(self):
+    def _close_login_window(self) -> None:
+        """
+        Close the login window
+        """
         if self.master.user:
             self.change_buttons_state(True)
         self.login_window.root.destroy()
         self.root.deiconify()
 
-    def _open_signup_window(self):
+    def _open_signup_window(self) -> None:
+        """
+        Open the signup window
+        """
         self.root.withdraw()
         self.signup_window = SignUp(self)
         self.signup_window.root.protocol('WM_DELETE_WINDOW', self._close_signup_window)
         self.signup_window.open()
 
-    def _close_signup_window(self):
+    def _close_signup_window(self) -> None:
+        """
+        Close the signup window
+        """
         if self.master.user:
             self.change_buttons_state(True)
         self.signup_window.root.destroy()
         self.root.deiconify()
 
-    def _open_settings(self):
+    def _open_settings(self) -> None:
+        """
+        Open the settings window
+        """
         self.root.withdraw()
         self.settings_window = Settings(self)
         self.settings_window.root.protocol('WM_DELETE_WINDOW', self._close_settings_window)
         self.settings_window.open()
 
-    def _close_settings_window(self):
+    def _close_settings_window(self) -> None:
+        """
+        Close the settings window.
+        """
         if not self.master.user:
             self.change_buttons_state(False)
         self.settings_window.root.destroy()
         self.root.deiconify()
 
-    def change_buttons_state(self, disable: bool):
+    def change_buttons_state(self, disable: bool) -> None:
+        """
+        Change the state of login and signup buttons.
+
+        Args:
+            disable (bool): True to disable buttons, False to enable.
+        """
         self.login_btn.config(state=NORMAL if not disable else DISABLED)
         self.signup_btn.config(state=NORMAL if not disable else DISABLED)
